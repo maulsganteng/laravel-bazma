e41a2278d4669a1e00ef2c11e6a779686addb69f
maulsganteng <muhammadfikri6910@smk.belajar.id>
2023-06-03 10:06:37 +0700

    push

---
+++ app/Http/Controllers/Pages/CategoriesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+use App\Models\CategoriesBooks;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    public function index()
+    {
+        $allCategories = \App\Models\CategoriesBooks::latest()->get();
+        return view("pages.categories.index",compact($allCategories));
+    }
+    public function create()
+    {
+        return view("pages.categories.create");
+    }
+    public function store(Request $request)
+    {
+    }
+    public function show(Request $request, string $id)
+    {
+    }
+    public function update(Request $request, string $id)
+    {
+    }
+    public function destroy($id)
+    {
+    }
+}
